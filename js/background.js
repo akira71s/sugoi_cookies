e524cd412e8fabb7193b56c04c273546c09a4ee8
Akira <akira.s7171@gmail.com>
2019-01-27 21:22:10 +0800

    remove subdomain before sending clear cookie query

--- js/background.js
+++ js/background.js
@@ -7,24 +7,14 @@
  */
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     let message = request.message;
-    let currentDomain = request.domain;
+    let currentDomain = request.domain.includes('www') ?
+      currentDomain.slice('www.'.length) : request.domain;
     if(message ==='clearCookies'){
       clearCookies_(currentDomain).then((result)=>{
         sendMsg_(result)
       });
     }
 });
-
-/**
- * @private 
- */
-function sendMsg_(msg){
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    // found active tab
-    const tabID = tabs[0].id;
-    chrome.tabs.sendMessage(tabID, {greeting: msg});
-  });
-};
 
 /**
  * @private 
@@ -35,11 +25,24 @@ function clearCookies_(currentDomain){
   return new Promise((resolve, reject)=>{ 
     chrome.cookies.getAll({domain: currentDomain}, function(cookies) {
       cookies.forEach(function(cookie){
-        console.log(cookie);
         let url = "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain + cookie.path;
         chrome.cookies.remove({"url": url, "name": cookie.name}, function(cookie){('deleted_cookie', cookie)});
       });
     });
-    resolve("cookieCleared");
-  })
+    resolve("cookieCleared")
+  });
+};
+
+/**
+ * @private 
+ */
+function sendMsg_(msg){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    // found active tab
+    if(!tabs[0]){
+      return;
+    }
+    const tabID = tabs[0].id;
+    chrome.tabs.sendMessage(tabID, {greeting: msg});
+  });
 };
