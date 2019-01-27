97d4e6a9a6229ec70c90cf3bd81a3033fd89d727
Akira <akira.s7171@gmail.com>
2019-01-27 10:40:42 +0800

    fixed bug for url change

--- js/content.js
+++ js/content.js
@@ -18,10 +18,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 function clearCookies_(){
     chrome.runtime.sendMessage({message:'clearCookies', domain:document.domain}, function(response){
     console.log(STYLE_ESCAPE + response.message, STYLE_BOLD);
-    console.log('reloading this page in 3 seconds...');
-    console.log(window.location.href);
-    setTimeout(window.location = getNewUrl(window.location.href), 30000);
+    console.log('reloading this page in 2 seconds...');
+    setTimeout(reload, 2000);
   });
+};
+
+function reload(){
+  window.location = getNewUrl(window.location.href);
 };
 
 /** 
@@ -30,9 +33,9 @@ function clearCookies_(){
  */
 function getNewUrl (url) {
   if(url.includes('?gclid')){
-    url.substring(url.indexOf('?gclid'),0);
+    url = url.substring(url.indexOf('?gclid'),0);
   } else if(url.includes('&gclid')) {
-    url.substring(url.indexOf('&gclid'),0);
+    url = url.substring(url.indexOf('&gclid'),0);
   }
   return url;
 };
