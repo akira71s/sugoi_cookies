7799ab06c4269e816b44ee213ab4ecccc2268060
Akira <akira.s7171@gmail.com>
2019-01-27 19:26:24 +0800

    integrate main.js into writter.js

--- js/writers.js
+++ js/writers.js
@@ -2,8 +2,22 @@
  * @author akira.s7171@gmail.com
  */
 
- /** 
- *  
+/** 
+ * @author <Akira Sakaguchi> akira.s7171@gmail.com
+ * 
+ * main scripts to show Google Ads Cookies to users 
+ */
+!function(){
+  let style = [STYLE_BOLD, STYLE_BLUE];
+  console.log("%cSUGOI!Cookies for Google Ads ⊂(・(ェ)・)⊃ ver.0.7.1", style.join(';'));
+  console.log("Your current domain is : 【", document.domain,"】");
+  write('_gcl_aw');
+  write('_gac');
+  console.log("%cDONE!", style.join(';'));
+}();
+
+/** 
+ *  calling console log
  */
 let write = function(cookieNm){
   /** @type {Promise} */
